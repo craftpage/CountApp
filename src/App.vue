000bfc3b47596eba7e48271165fc833a4e8c8cc9
craftpage <craftpage@gmail.com>
2021-05-23 22:35:38 +0900

    ok

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>CountApp</h1>
+  <CountApp />
+</template>
+
+<script setup>
+import CountApp from './components/CountApp.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CountApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>{{ num }}</p>
+  <button v-on:click="up" class="btn btn-primary">Up</button>　
+  <button v-on:click="reset" class="btn btn-dark">Reset</button>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      num: 0
+    }
+  },
+  methods:{
+    up(){
+      this.num++
+    },
+    reset(){
+      this.num = 0
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+p{
+  font-size: 1000%;
+}
+
+</style>
