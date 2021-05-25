beb8e8911fa76c8b1b4181f0e01a4f24237a103b
craftpage <craftpage@gmail.com>
2021-05-25 22:35:56 +0900

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 本番時はgithubリポジトリをルートパスにする
+  base: process.env.NODE_ENV === 'production' ? '/CountApp/' : './',
+  resolve: {
+    // @/ によるimportをできるようにする。
+    alias: {
+      '@': path.resolve('__dirname', '/src')
+    }
+  },
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue()]
-})
+});
+
